6a7cc6590cff6c327d90de3a277080309e5a049e
Greg Talbott <gtalbott64@gmail.com>
2021-02-07 22:49:20 -0700

    main

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,52 +10,51 @@ with open(csvpath, newline='') as csvfile:
 
 
 # Variables
-    candidate = ["Khan", "O'Tooley", "Li", "Correy"]
-    percentage = 0
-    total_votes = 0
-    candidate_vote_counter = Counter
-    
+    candidate = []
+    candidate_list = []
+    count = 0
+    percentage = []
+    number_votes = 0
+    candidate_vote_counter = []
+    x = {"Khan", "Correy", "Li", "O'Tooley"}
 
 
 # Why is this action different from PyBank - row = next(csvreader)
     for row in csvreader:
        
-        total_votes += 1
+        number_votes += 1
 
 # Return a list of candidate votes      
-        candidate_list = (row[2])
+        candidate_list.append(row[2])
+
         # step through candidate list
-        if candidate in candidate_list
-           candidate_index = candidate.index(candidate_list)
-           candidate_vote_counter[candidate_index] = candidate_vote_counter[candidate_index] + 1
-        else
-           
- # Calculate the total number of votes each candidate won          
-           candidate_list.append(candidate)
-           candidate_vote_counter.append(1)
+    for x in set(candidate_list):
+        candidate.append(x)   
+        
+ # Calculate the total number of votes each candidate won  
+ """ This needs to be Total votes / "Khan" votes * 100
+ Total votes / "Correy" votes * 100 
+ Total votes / "Li" votes * 100 
+ Total votes / "O'Tooley"  * 100"""     
+        y = candidate_list.count(x)
+        candidate_vote_counter.append(y)
 
-# Calculate percentage of votes for each candidate
-           vote_tally = candidate_vote_counter[0]
-           vote_tally_index = 0
-
-    for count in range(len(candidate_list)):
-        percentage = candidate_vote_counter[count]/total_votes*100
-        total_percentage.append(percentage)
-        if candidate_vote_counter[count] > vote_tally:
-            vote_tally - candidate_vote_counter[count]
-            print vote_tally
-            vote_tally_index = count
+# Calculate the percentage of the votes for each candidate 
+        z = (y/number_votes)*100
+        percentage.append(z)
     
-    winner = candidate_list[vote_tally_index]
+    winning_count = max(candidate_vote_counter)
+    winner = candidate.index(winning_count)
 
     print(f'Election Results')
     print(f'-------------------------------------------')
-    print(f'Total Votes: {total_votes}')
+    print(f'Total Votes: {number_votes}')
+    
     for count in range(len(candidate_list)):
-        print(f'{candidate_list[count]}: {percentage[count]}% ({candidate_vote_counter[count]}')
-    print(f'-------------------------------------------')
-    print(f'Winner: {winner}')
-    print(f'--------------------------------------------')
+        print(f'{candidate_list[x]}: {percentage[z]}% ({candidate_vote_counter[y]})')
+        print(f'-------------------------------------------')
+        print(f'Winner: {winner}')
+        print(f'-------------------------------------------')
 
 
     # Output files
@@ -67,10 +66,11 @@ with open(csvpath, newline='') as csvfile:
 
         txtfile.write(f'Election Results')
         txtfile.write(f'-------------------------------------------')
-        txtfile.write(f'Total Votes: {total_votes}')
-          for count in range(len(candidate_list)):
-              txtfile.write(f'{candidate_list[count]}: {percentage[count]}% ({candidate_vote_counter[count]}')
+        txtfile.write(f'Total Votes: {number_votes}')
+    
+    for count in range(len(candidate_list)):
+        txtfile.write(f'{candidate_list[x]}: {percentage[z]}% ({candidate_vote_counter[y]})')
         txtfile.write(f'-------------------------------------------')
         txtfile.write(f'Winner: {winner}')
-        txtfile.write(f'--------------------------------------------')
+        txtfile.write(f'-------------------------------------------')
     
